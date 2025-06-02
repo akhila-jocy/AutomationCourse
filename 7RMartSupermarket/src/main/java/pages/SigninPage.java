912e6ea4b274a51caeee4c4d4eb691e0f487af6c
Admin <Admin@192.168.1.35>
2025-06-03 00:35:16 +0530

    Signin_automation

---
+++ 7RMartSupermarket/src/main/java/pages/SigninPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SigninPage {
+
+	
+	public WebDriver driver;
+	
+	public SigninPage(WebDriver driver)
+	{
+		this.driver = driver;
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(xpath = "//input[@name='username']") private WebElement usernameField;
+	@FindBy(xpath = "//input[@name='password']") private WebElement passwordField;
+	@FindBy(xpath = "//button[@type='submit']") private WebElement signinButton;
+	
+	
+	public void enterUsernameOnUsernameField(String username)
+	{
+		usernameField.sendKeys(username);
+	}
+	public void enterPasswordOnpasswordField(String password)
+	{
+		passwordField.sendKeys(password);
+	}
+	public void clickOnSigninButton()
+	{
+		signinButton.click();
+	}
+}
---
+++ 7RMartSupermarket/src/test/java/testScript/SigninTest.java
@@ -0,0 +1,58 @@
+package testScript;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import automationCore.Base;
+import pages.SigninPage;
+import utilities.ExcelUtility;
+
+public class SigninTest extends Base{
+	
+	@Test
+	public void verifyUserSigninWithValidCredentials() throws IOException
+	{
+		String username = ExcelUtility.getStringData(0, 0,"SigninPage");
+		String password = ExcelUtility.getStringData(0, 1,"SigninPage");
+		SigninPage signinPage = new SigninPage(driver);
+		signinPage.enterUsernameOnUsernameField(username);
+		signinPage.enterPasswordOnpasswordField(password);
+		signinPage.clickOnSigninButton();
+		
+	}
+	
+	@Test
+	public void  verifyUserSigninWithValidUsernameAndInvalidPassword() throws IOException
+	{
+		String username = ExcelUtility.getStringData(1, 0,"SigninPage");
+		String password = ExcelUtility.getStringData(1, 1,"SigninPage");
+		SigninPage signinPage = new SigninPage(driver);
+		signinPage.enterUsernameOnUsernameField(username);
+		signinPage.enterPasswordOnpasswordField(password);
+		signinPage.clickOnSigninButton();	
+		
+	}
+	
+	@Test
+	public void  verifyUserSigninWithInvalidUsernameAndValidPassword() throws IOException
+	{
+		String username = ExcelUtility.getStringData(2, 0,"SigninPage");
+		String password = ExcelUtility.getStringData(2, 1,"SigninPage");
+		SigninPage signinPage = new SigninPage(driver);
+		signinPage.enterUsernameOnUsernameField(username);
+		signinPage.enterPasswordOnpasswordField(password);
+		signinPage.clickOnSigninButton();
+	}
+	
+	@Test
+	public void verifyUserSigninWithInvalidCredentials() throws IOException
+	{
+		String username = ExcelUtility.getStringData(0, 0,"SigninPage");
+		String password = ExcelUtility.getStringData(0, 1,"SigninPage");
+		SigninPage signinPage = new SigninPage(driver);
+		signinPage.enterUsernameOnUsernameField(username);
+		signinPage.enterPasswordOnpasswordField(password);
+		signinPage.clickOnSigninButton();
+	}
+}
